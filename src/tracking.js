133e5d9a0605ec2382168155226ede4445d65a66
Krzysztof Madej <krzysztof.madej.1987@gmail.com>
2023-06-10 12:44:50 +0200

    Testing dev staging

---
+++ src/tracking.js
@@ -0,0 +1,10 @@
+const cookie3Options = { "siteId": 1, "additionalTracking": true, "cookielessEnabled": true };
+
+var _paq = window._paq = window._paq || [];
+(function () {
+    var d = document, g = d.createElement('script'), s = d.getElementsByTagName('script')[0];
+    g.async = true;
+    g.src =
+        'https://cdn.dev-staging.cookie3.co/scripts/analytics/latest/cookie3.analytics.min.js';
+    s.parentNode.insertBefore(g, s);
+})();
